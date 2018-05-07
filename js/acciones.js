4764955278eb0948d57ee9588af7c33ee3d40a22
AdanCalixtoAhedo <cb16.calixto.ahedo.adan@gmail.com>
2018-05-07 16:19:47 -0500

    3subida
    Quinta Aplicacion

--- js/acciones.js
+++ js/acciones.js
@@ -2,7 +2,7 @@
 $(document).ready(function(e){
 	document.addEventListener("deviceready", function(){
 		$('#sonar').tap(function(){
-			navigator.notification.bee(2);
+			navigator.notification.beep(2);
 		});//Cierre del beep
 		$('#vibrar').tap(function(){
 			navigator.notification.vibrate(2000);
